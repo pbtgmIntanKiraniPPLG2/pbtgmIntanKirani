1de2c38fb7ce46a616e69e5c7aaed842c67af0a9
pbtgmIntanKiraniPPLG2 <enzycelestia@gmail.com>
2024-08-25 11:34:40 +0700

    Add files via upload

---
+++ pertemuan4tugas2/src/main/java/com/mycompany/pertemuan4tugas2/Pertemuan4tugas2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4tugas2;
+
+/**
+ *
+ * @author intan
+ */
+public class Pertemuan4tugas2 {
+
+    public static void main(String[] args) {
+        int w = 5; /* 11 = 00001011 */
+        int x = 6;
+        int y = 8;
+        int z = 8;/* 22 = 00010110 */
+        
+ System.out.println("x=6 ;w=5;");
+ System.out.println("y=8-x++;"); 
+ System.out.println("z= -++w ");
+
+ }
+}
+
