04453646c032fb3f170a6a8fa6061cdb7762f9b3
pbtgmIntanKiraniPPLG2 <enzycelestia@gmail.com>
2024-08-25 11:10:41 +0700

    Add files via upload

---
+++ pertemuan4tugas1/src/main/java/com/mycompany/pertemuan4tugas1/Pertemuan4tugas1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4tugas1;
+
+/**
+ *
+ * @author intan
+ */
+public class Pertemuan4tugas1 {
+
+
+ public static void main(String args[]) {
+ int angka1 = 10;
+ int angka2 = 3;
+System.out.println("bilangan1 + bilangan2: " + 
+(angka1 + angka2) );
+System.out.println("bilangan1 - bilangan2: " + 
+(angka1 - angka2) );
+System.out.println("bilangan1 * bilangan2: " + 
+(angka1 * angka2) );
+System.out.println("bilangan1 / bilangan2: " + 
+(angka1 / angka2) );
+System.out.println("bilangan1 % bilangan2: " + 
+(angka1 % angka2) );
+ }
+}
